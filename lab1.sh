47b67b5ddb4d8e92791e8a5f403213f321684260
Dan Micheli <michelison.daniel@gmail.com>
2017-04-04 15:14:16 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -7,14 +7,16 @@ ls                              # Tells me what files are in the directory. (WHA
 cd                              # Changes the current working directory. (WHAT)
 date                            # What time is it on the machine. (WHEN)
 which <directory name>          # Displays directory location must be accesible from current working directory
+vi <filename.sh>                # Allows access to vi text editor with a file name availble for use with the shell.
+
 
 
 
 /var/root # sh test1.sh                                                         
-test1.sh: line 1: !#/bin/sh: not found                                          
+test1.sh: line 1: #!/bin/sh: not found                                          
 My gosh, the moon has fallen from the sky.                                      
 /var/root # cat test1.sh                                                        
-!#/bin/sh                                                                       
+#!/bin/sh                                                                       
                                                                                 
 echo "My gosh, the moon has fallen from the sky."                                                
                                                                                 
