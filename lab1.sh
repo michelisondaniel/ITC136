4a942f93a570817f0c84f5fae38939648309a3ea
Dan Micheli <michelison.daniel@gmail.com>
2017-04-04 15:10:55 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,9 +1,22 @@
 # This is a comment
 Linux Commands
 
-pwd           # Prints working directory and tells me where I am. (WHERE)
-whoami        # Displays the current logged in user. (WHO)
-ls            # Tells me what files are in the directory. (WHAT)
-cd            # Changes the current working directory. (WHAT)
-date          # What time is it on the machine. (WHEN)
+pwd                             # Prints working directory and tells me where I am. (WHERE)
+whoami                          # Displays the current logged in user. (WHO)
+ls                              # Tells me what files are in the directory. (WHAT)
+cd                              # Changes the current working directory. (WHAT)
+date                            # What time is it on the machine. (WHEN)
+which <directory name>          # Displays directory location must be accesible from current working directory
+
+
+
+/var/root # sh test1.sh                                                         
+test1.sh: line 1: !#/bin/sh: not found                                          
+My gosh, the moon has fallen from the sky.                                      
+/var/root # cat test1.sh                                                        
+!#/bin/sh                                                                       
+                                                                                
+echo "My gosh, the moon has fallen from the sky."                                                
+                                                                                
+
 
