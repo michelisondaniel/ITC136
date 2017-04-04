1d323b5aa0ac104c093b10ba226448ba591b60b2
Dan Micheli <michelison.daniel@gmail.com>
2017-04-04 14:47:47 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,9 @@
 # This is a comment
+Linux Commands
+
+pwd           # Prints working directory and tells me where I am. (WHERE)
+whoami        # Displays the current logged in user. (WHO)
+ls            # Tells me what files are in the directory. (WHAT)
+cd            # Changes the current working directory. (WHAT)
+date          # What time is it on the machine. (WHEN)
+
